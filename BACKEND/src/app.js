0452aae9c31c3d3b0f7cd342e4b27b13aab2b408
Zisan412 <shaikh.zisan412@gmail.com>
2026-03-29 21:42:34 +0530

    app changes test

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const connectDB = require("../db/db");
-const env = require("dotenv")
+// const env = require("dotenv")
 const cors = require("cors")
-
-env.config()
+require("dotenv").config();
+// env.config()
 
 const register = require("../routes/user.route");
 const app = express();
@@ -14,7 +14,8 @@ app.use(cors())
 app.use(express.json());
 
 
-
+console.log("JWT:", process.env.JWT_TOKEN);
+console.log("MONGO:", process.env.MONGO_URI);
 // app.use("/user",register)
 
 // app.get("/", (req, res) => {
